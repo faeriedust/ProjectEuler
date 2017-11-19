4bac9a450603ffe883db403e12a4822f8496b092
Elijah Kohrt <elijah.kohrt@blocherconsulting.com>
2017-11-19 22:57:12 +0900

    nice

--- problems/ProjectEuler.Core.Tests/UnitTesting/DivideByRoundUpTests.cs
+++ problems/ProjectEuler.Core.Tests/UnitTesting/DivideByRoundUpTests.cs
@@ -10,14 +10,12 @@ namespace ProjectEuler.Core.Tests.UnitTesting {
 
     public static IEnumerable<object[]> TestData() =>
       _TestData_Raw()
-        .Select(x => new object[] { x.Step, x.UpperBound, x.Nearest })
+        .Select(x => new object[] { x.Numerator, x.Denominator, x.Quotient })
         .ToArray();
 
 
-    private static IEnumerable<(long Step, long UpperBound, long Nearest)> _TestData_Raw() {
-      yield return (1, 100, 100);
-      yield return (3, 100, 99);
-      yield return (5, 100, 100);
+    private static IEnumerable<(long Numerator, long Denominator, long Quotient)> _TestData_Raw() {
+      yield return (5, 3, 2);
     }
   }
 }
