f00769fcf47620eab87c65cd5ab62cc648fbebbe
Elijah Kohrt <elijah.kohrt@blocherconsulting.com>
2017-11-19 19:47:27 +0900

    here

--- problems/Problem1.Answer/Problem1.cs
+++ problems/Problem1.Answer/Problem1.cs
@@ -1,7 +1,7 @@
 ﻿using System.Linq;
 
 namespace Problem1.Answer {
-  public static class Problem1 {
+  public static class Problem1Answer {
     public static int SumMultiplesOfThreeOrFiveLessThan(int cap) =>
       Enumerable.Range(0, cap)
         .Where(i => i % 3 == 0 || i % 5 == 0)
--- problems/Problem1.Tests/Problem1Tests.cs
+++ problems/Problem1.Tests/Problem1Tests.cs
@@ -1,16 +1,16 @@
-﻿using Xunit;
-using Problem1.Answer;
+﻿using Problem1.Answer;
+using Xunit;
 
 namespace Problem1.Tests {
   public class Problem1Tests {
     [Fact]
     public void Problem1_SumOfMultiplesOfThreeOrFiveLessThan10_Is23() {
-      Assert.Equal(23, Problem1.SumMultiplesOfThreeOrFiveLessThan(10));
+      Assert.Equal(23, Problem1Answer.SumMultiplesOfThreeOrFiveLessThan(10));
     }
 
     [Fact]
     public void Problem1_SumOfMultiplesOfThreeOrFiveLessThan1000_Is233168() {
-      Assert.Equal(233168, Problem1.SumMultiplesOfThreeOrFiveLessThan(1000));
+      Assert.Equal(233168, Problem1Answer.SumMultiplesOfThreeOrFiveLessThan(1000));
     }
   }
 }
