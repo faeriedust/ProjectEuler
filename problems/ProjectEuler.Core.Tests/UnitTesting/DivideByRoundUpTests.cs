90056cbc4d42b755e89e143460ad16052b5c8cd5
Elijah Kohrt <elijah.kohrt@blocherconsulting.com>
2017-11-19 22:55:39 +0900

    LOL

---
+++ problems/ProjectEuler.Core.Tests/UnitTesting/DivideByRoundUpTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace ProjectEuler.Core.Tests.UnitTesting {
+  public class DivideByRoundUpTests {
+    [Theory, MemberData(nameof(TestData))]
+    public void DivideByRoundUp(long numerator, long denominator, long expectedResult) =>
+      Assert.Equal(expectedResult, numerator.DivideByRoundUp(denominator));
+
+    public static IEnumerable<object[]> TestData() =>
+      _TestData_Raw()
+        .Select(x => new object[] { x.Step, x.UpperBound, x.Nearest })
+        .ToArray();
+
+
+    private static IEnumerable<(long Step, long UpperBound, long Nearest)> _TestData_Raw() {
+      yield return (1, 100, 100);
+      yield return (3, 100, 99);
+      yield return (5, 100, 100);
+    }
+  }
+}
